2c763c21be75bfbd6e50427468a2f7e1c3695b88
brian kanari <briangitui26@gmail.com>
2024-06-13 10:20:46 +0300

    completes

--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,13 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
-
+  function handleChangeColor() {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);}
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child onChangeColor={handleChangeColor} />
+      <Child onChangeColor={handleChangeColor}/>
     </div>
   );
 }
